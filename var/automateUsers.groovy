14ffef708053edd7698dcd4a680c1247ee0feda5
Prashanth <prashanthh727@gmail.com>
2019-08-25 10:52:56 +0530

    Added skeleton code in groovy

--- var/automateUsers.groovy
+++ var/automateUsers.groovy
@@ -1,1 +1,35 @@
 //Add Code here
+def call(userName,role,email){
+
+	pipeline{
+		agent any
+		stages{
+			stage("verify users"){
+				steps{
+					script{
+						echo "pwd && ls -l"
+						cleanWs();
+						echo "Hi Groovy";
+						echo "pwd && ls -l"
+					}
+				}
+			}
+			stage("Create users"){
+				steps{
+					script{
+						echo "Creating users";
+						echo "$userName ($role) - $email"
+						echo "Lets call API from here using groovy class";
+					}
+				}
+			}
+		}
+		post{
+			always{
+				script{
+					echo "Notify stake holders here";
+				}
+			}
+		}
+	}
+}
